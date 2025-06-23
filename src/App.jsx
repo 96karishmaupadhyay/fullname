6869dc21716084b5598ccda4536a5fec788c9e22
96karishmaupadhyay <karishmaupadhyay1996@gmail.com>
2025-06-23 18:43:55 +0530

    fd

--- src/App.jsx
+++ src/App.jsx
@@ -20,15 +20,20 @@ const App = () => {
   return (
     <div className='main'>
     <form onSubmit={handleSubmit} className='form'>
+      <h1>Full Name Display</h1>
+      <label htmlFor="">First Name</label>
       <input
         type="text"
         placeholder="First Name"
         value={fName}
         onChange={(e) => setfName(e.target.value)}
+        required
       />
       <br />
+      <label htmlFor="">Last Name</label>
       <input
         type="text"
+        required
         placeholder="Last Name"
         value={lName}
         onChange={(e) => setlName(e.target.value)}
